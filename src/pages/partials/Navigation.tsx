89ea7bd361847ad976124472e216be95ae915aa1
amanda <amanda@swedsec.se>
2026-04-07 21:25:09 +0200

    only show Todos menu item active when we're on the page with all todos, not a single todo

--- src/pages/partials/Navigation.tsx
+++ src/pages/partials/Navigation.tsx
@@ -10,7 +10,7 @@ export const Navigation = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/todos">Todos</Nav.Link>
+            <Nav.Link as={NavLink} to="/todos" end>Todos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
